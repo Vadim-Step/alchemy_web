43cb55277b1460c5d0789f0cc6378ad8eed1774c
Vadim-Step <BeJIuKaH1@yandex.ru>
2021-03-06 17:36:13 +0300

    lesson 3 task 7

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from flask import Flask
-import datetime
-from data.job import Jobs
+from flask import Flask, render_template
 from data import db_session
+from sqlalchemy import orm
+
+from data.db_session import create_session
+from data.job import Jobs
+from data.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init("db/blogs.db")
-    # app.run()
+    app.run()
+
+
+@app.route('/tab1')
+def table():
+    db_session.global_init('db/blogs.db')
+    db_sess = create_session()
+    lst = []
+    for job in db_sess.query(Jobs).all():
+        row = [job.job]
+        for i in db_sess.query(User).all():
+            print(i)
+            if i.id == job.team_leader:
+                row.append(f'{i.name} {i.surname}')
+        row.append(job.work_size)
+        row.append(job.collaborators)
+        if job.is_finished:
+            row.append('Is finished')
+        else:
+            row.append('Is not finished')
+        lst.append(row)
+    return render_template('index.html', lst=lst)
 
 
 if __name__ == '__main__':
-    main()
-    job = Jobs()
-    job.team_leader = 1
-    job.job = 'deployment of residential modules 1 and 2'
-    job.work_size = 15
-    job.collaborators = '2, 3'
-    job.start_date = datetime.datetime.now()
-    job.is_finished = False
-    db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
+    app.run(port=8000, host='127.0.0.1')
