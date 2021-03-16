1c4ff99355e383342817116f8345bc036161c9be
Vadim-Step <BeJIukaH1@yandex.ru>
2021-03-16 17:58:45 +0300

    addadd

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 from flask_login import login_user, login_required
 from flask_restful import Api
@@ -257,4 +259,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
